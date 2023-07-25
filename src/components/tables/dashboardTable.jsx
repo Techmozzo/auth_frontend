0f552c36ffc7d2c49ed4216d75072ba5ba256980
mencraft <ekemboy@gmail.com>
2023-07-25 22:21:22 +0800

    add new buttons

--- src/components/tables/dashboardTable.jsx
+++ src/components/tables/dashboardTable.jsx
@@ -10,14 +10,19 @@ import TableRow from '@mui/material/TableRow';
 import { Box } from '@mui/material';
 import { useHistory } from 'react-router';
 import { CgArrowsExpandDownLeft, CgArrowsExpandUpRight } from 'react-icons/cg';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import { IconButton } from '@material-ui/core';
-import Menu from '@mui/material/Menu';
+// import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { MdOutlineMoreVert } from 'react-icons/md';
+import { Button } from 'primereact/button';
+import { Menu } from 'primereact/menu';
+import { ConfirmPopup, confirmPopup } from 'primereact/confirmpopup';
+import { Toast } from 'primereact/toast';
 import usePermission from '../hooks/usePermission';
 import { sentenceCaps } from '../../utilities/stringOperations';
+import { del } from '../../services/fetch';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -41,23 +46,73 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 export default function DashboardTable({ data }) {
   const editEnganagment = usePermission('edit-engagement');
-
+  const menuLeft = React.useRef(null);
   const { push } = useHistory();
-  function createData(name, date, client, members, status, action) {
+  const toast = React.useRef(null);
+  const [loading, setLoading] = React.useState(false);
+  function createData(name, date, client, members, status, id) {
     return {
-      name, date, client, members, status, action
+      name,
+      date,
+      client,
+      members,
+      status,
+      id,
+      action: [
+        {
+          label: 'Options',
+          items: [
+            {
+              label: 'View Engangment',
+              icon: 'pi pi-refresh',
+              command: () => {
+                console.log('CLicked');
+                console.log(id);
+                // viewRow(id);
+                // toast.current.show({
+                //   severity: 'success', summary: 'Updated', detail: 'Data Updated', life: 3000
+                // });
+              }
+            },
+            {
+              label: 'Edit Engangment',
+              icon: 'pi pi-times',
+              command: () => {
+                console.log('CLicked');
+                // toast.current.show({
+                //   severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000
+                // });
+              }
+            },
+            {
+              label: 'Delete Engangment',
+              icon: 'pi pi-times',
+              command: () => {
+                console.log('CLicked');
+                handleRow(id);
+                // toast.current.show({
+                //   severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000
+                // });
+              }
+            }
+          ]
+        }
+      ]
+
     };
   }
   const rows = data?.map((item) => createData(
-    item?.name, item?.year, item?.client?.name, item?.team_members_count, item?.status?.name
+    item?.name, item?.year, item?.client?.name, item?.team_members_count, item?.status?.name, item?.id
   ));
+  console.log('Row ', data);
   const handleRow = (row) => {
-    const theData = data.filter((item) => item.name === row.name);
-    push(`/app/engagement/engagement/${theData[0].id}`);
+    // const theData = data.filter((item) => item.name === row.name);
+    push(`/app/engagement/engagement/${row}`);
   };
   const viewRow = (row) => {
-    const theData = data.filter((item) => item.name === row.name);
-    push(`/app/engagement/view/${theData[0].id}`);
+    // const theData = data.filter((item) => item.name === row.name);
+    push(`/app/engagement/view/${row}`);
+    console.log('cliers', row);
   };
   // console.log('Den ', data);
   const ITEM_HEIGHT = 48;
@@ -68,6 +123,29 @@ export default function DashboardTable({ data }) {
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+  const acceptDelete = async (id) => {
+    setLoading(true);
+    try {
+      const res = await del({ endpoint: 'ENGAGEMENT', param: id, auth: true });
+      console.log(res);
+      toast.current.show({
+        severity: 'info', summary: 'Confirmed', detail: 'Engangment deleted', life: 3000
+      });
+      setLoading(false);
+      window.location.reload(false);
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
+    }
+  };
+  const confirm1 = (event, id) => {
+    confirmPopup({
+      target: event.currentTarget,
+      message: 'Are you sure you want to proceed?',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => acceptDelete(id)
+    });
   };
   return (
     <TableContainer component={Box}>
@@ -93,63 +171,20 @@ export default function DashboardTable({ data }) {
               <StyledTableCell align="right"><div className="theme-font-2">{sentenceCaps(row.members)}</div></StyledTableCell>
               <StyledTableCell align="right"><div className="theme-font-2">{sentenceCaps(row.status)}</div></StyledTableCell>
               <StyledTableCell align="right">
-
-                <div>
-                  <IconButton
-                    aria-label="more"
-                    id="long-button"
-                    aria-controls="long-menu"
-                    aria-expanded={open ? 'true' : undefined}
-                    aria-haspopup="true"
-                    onClick={handleClick}
-                  >
-                    {/* <MoreVertIcon /> */}
-                    <MdOutlineMoreVert />
-                  </IconButton>
-                  <Menu
-                    id="long-menu"
-                    MenuListProps={{
-                      'aria-labelledby': 'long-button'
-                    }}
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    PaperProps={{
-                      style: {
-                        maxHeight: ITEM_HEIGHT * 4.5,
-                        width: '20ch'
-                      }
-                    }}
-                  >
-
-                    <MenuItem onClick={() => viewRow(row)}>
-                      View Engagement
-                    </MenuItem>
-                    {editEnganagment
+                <Menu model={row.action} popup ref={menuLeft} id="popup_menu_left" />
+                <Toast ref={toast} />
+                <ConfirmPopup />
+                <Button icon="pi pi-eye" className="mr-2" rounded text onClick={() => viewRow(row.id)} aria-controls="popup_menu_left" aria-haspopup />
+                {editEnganagment
                   && (
-                    <MenuItem onClick={() => handleRow(row)}>
-                      Edit Engagement
-                    </MenuItem>
-                  ) }
-                  </Menu>
-                </div>
-                {/* <div className="theme-font-2">
+                    <>
+                      <Button icon="pi pi-file-edit" className="mr-2" rounded text onClick={() => handleRow(row.id)} aria-controls="popup_menu_left" aria-haspopup />
+                      {loading
+                        ? <i className="pi pi-spin pi-spinner mr-2" />
+                        : <Button icon="pi pi-trash" className="mr-2" type="button" rounded text onClick={(e) => confirm1(e, row.id)} style={{ color: 'red' }} aria-controls="popup_menu_left" aria-haspopup /> }
 
-                  <Button type="button" className="btn-small btn text-black" onClick={() => viewRow(row)}>
-
-                    View Engagement
-                  </Button>
-                </div>
-                <div className="theme-font-2">
-                  {editEnganagment
-                  && (
-                    <Button type="button" className="btn-small btn " style={{ color: 'black' }} onClick={() => handleRow(row)}>
-
-                      Edit Engagement
-
-                    </Button>
-                  ) }
-                </div> */}
+                    </>
+                  )}
               </StyledTableCell>
             </StyledTableRow>
           ))}
